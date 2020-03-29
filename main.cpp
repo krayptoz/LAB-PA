df237b3d6d37bc209635b8f5f8068e635263cd5f
FDelpoggio <55247515+FDelpoggio@users.noreply.github.com>
2020-03-29 14:55:26 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+using namespace std;
+// Data type
+
+class Dtfecha{
+public:
+    int dia;
+    int mes;
+    int anio;
+};
+
+class DtViajeBase{
+public:
+    Dtfecha fecha;
+    int duracion;
+    int distancia;
+};
+
+enum class TipoBici{
+    PASEO,MONTANIA
+};
+
+class DtVehiculo{
+public:
+    int nroSerie;
+    float porcentaje;
+    float precioBase;
+};
+
+class DtMonopatin: public DtVehiculo{
+public:
+        bool tieneLuces;
+};
+
+class DtBicicleta: DtVehiculo{
+public:
+    TipoBici tipo;
+    int cantCambios;
+};
+
+class DtViaje: public DtViajeBase{
+public:
+    float precioTotal;
+    DtVehiculo vehiculo;
+};
+
+
+// Clases
+
+class Usuario{
+public:
+    string cedula;
+    string nombre;
+    Dtfecha fechaIngreso;
+};
+
+class Viaje{
+public:
+    Dtfecha fecha;
+    int duracion;
+    int distancia;
+};
+
+class Vehiculo: public Viaje{
+private:
+    int nroSerie;
+    float porcentajeBateria;
+    float precioBase;
+public:
+   float darPrecioViaje(int duracion, int distancia);
+};
+
+float Vehiculo::darPrecioViaje(int,int){
+    if(Monopatin::tieneLuces){
+        return 0.5 * duracion + distancia * precioBase;
+    }
+    else{
+        return distancia * precioBase;
+    }
+}
+
+class Monopatin: public DtMonopatin{
+private:
+    bool tieneLuces;
+public:
+    float darPrecioViaje(int duracion, int distancia);
+};
+
+class Bicicleta: public DtBicicleta{
+private:
+    TipoBici tipo;
+    int cantCambios;
+public:
+    float darPrecioViaje(int duracion, int distancia);
+};
+
+
+
+int main()
+{
+
+    return 0;
+}
